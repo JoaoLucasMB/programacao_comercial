d6a8d9844c6949d51c894fd0cfede4fc96504e28
JoaoLucasMB <jaum998@gmail.com>
2020-11-29 17:55:28 -0300

    Adicionando links nas páginas para conectar as páginas existentes no momento

--- veiculos/urls.py
+++ veiculos/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from veiculos import views
+# from django.conf import settings
+# from django.conf.urls.static import static
 urlpatterns = [
     path('', views.VeiculosList.as_view(), name='listar-veiculos'),
     path('novo/', views.VeiculosNew.as_view(), name='novo-veiculo'),
     path('<int:pk>/', views.VeiculosEdit.as_view(), name='editar-veiculo'),
-]
+] 
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
